64d329f216f3ef6ae7f1e5de7c2c1c020bfce953
Akash Deep <akashdeep6632@gmail.com>
2026-01-29 12:22:15 +0530

    added new logic

--- Array/mergeSroted.cpp
+++ Array/mergeSroted.cpp
@@ -16,6 +16,19 @@ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
             nums1[i]=nums2[i-n];
         }
 }
+void merge3(vector<int> &nums1,int m, vector<int> &nums2,int n){
+    int j=0;
+    for(int k=0;k<m+n;k++){
+        if(k==m) j=0;
+        if(nums1[k]>nums2[j]){
+            swap(nums1[k],nums2[j]);
+            j++;
+        }
+        if(k>=m) swap(nums1[k],nums2[j++]);
+    }
+    // for(int k:nums1) cout<<k<<" ";
+
+}
 
 void merge2(vector<int>  &nums1,int m,vector<int> &nums2,int n){
     int i=m-1,j=n-1,k=m+n-1;
@@ -35,7 +48,7 @@ void merge2(vector<int>  &nums1,int m,vector<int> &nums2,int n){
 int main(){
     vector<int> arr={1,2,3,0,0,0};
     vector<int> arr2={2,5,6};
-    merge(arr,3,arr2,3);
+    merge3(arr,3,arr2,3);
     for(int i:arr){
         cout<<i<<" ";
     }

3bc01c8703fe1dbb0c18b5e591808ba1a5875d90
Akash Deep <akashdeep6632@gmail.com>
2026-01-31 10:05:37 +0530

    Rechecked code

--- Recursion/binarySearch.cpp
+++ Recursion/binarySearch.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 bool binarySearch(int *arr,int size,int key){
-    if(size==0) return false;
+    if(size<=0) return false;
     int mid=(size-1)/2;
     if(arr[mid]==key) return true;
     else if(arr[mid]>key) return binarySearch(arr,mid-1,key);
     else return binarySearch(arr+mid+1,size-mid,key);
 }
 int main(){
-    int arr[5]={1,2,3,4,5};
-    cout<<binarySearch(arr,5,5);
+    int arr[10]={3,5,7,8,9,12,14,17,20,22};
+    cout<<binarySearch(arr,10,22);
 }

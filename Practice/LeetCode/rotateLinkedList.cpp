6e80358b76b3b5f75789cb5fb781a5ef8b871e80
Akash Deep <akashdeep6632@gmail.com>
2026-02-07 12:25:44 +0530

    Rotating linked list

---
+++ Practice/LeetCode/rotateLinkedList.cpp
@@ -0,0 +1,23 @@
+ListNode* rotate(ListNode* head, int k) {
+    if(k==0) return head;
+    ListNode* temp=head;
+    ListNode* prev=temp;
+    while(temp->next!=NULL) {
+        prev=temp;
+        temp=temp->next;
+    }
+    prev->next=NULL;
+    temp->next=head;
+    return rotateRight(temp,k-1);
+}
+ListNode* rotateRight(ListNode* head, int k){
+    if(!head || !head->next) return head;
+    int count=0;
+    ListNode* temp=head;
+    while(temp!=NULL){
+        count++;
+        temp=temp->next;
+    }
+    k=k%count;
+    return rotate(head,k);
+}

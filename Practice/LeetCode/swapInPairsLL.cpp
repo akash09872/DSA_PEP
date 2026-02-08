f0f7b328892402c8ce8ff41319c1926802aa87cd
Akash Deep <akashdeep6632@gmail.com>
2026-02-08 09:16:34 +0530

    Cleaned code

--- Practice/LeetCode/swapInPairsLL.cpp
+++ Practice/LeetCode/swapInPairsLL.cpp
@@ -1,5 +1,13 @@
 // leetcode.com/problems/swap-nodes-in-pairs/submissions
-
+#include<iostream>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 ListNode* swapPairs(ListNode* head) {
     if(!head || !head->next) return head;
     ListNode* temp=head;

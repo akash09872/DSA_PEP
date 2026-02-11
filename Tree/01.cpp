4daef625097f2e82c20dd910b9132bb5e589b739
Akash Deep <akashdeep6632@gmail.com>
2026-02-12 01:43:10 +0530

    Started Tree Concepts

---
+++ Tree/01.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+/* 
+    Tree-> It is a non linear, hierarchical data structure consisting of nodes connected by edges.
+    Binary Tree-> A tree is a binary tree when every node has at most 2 children;
+    Terminologies:
+        *Leaf-> A node of a tree which has no child;
+        *Internal Node -> If a node is not leaf / it's
+        *Siblings -> The children of same parent
+        *Length Path -> Length of path is 1 less than the numver of nodes on the path
+*/

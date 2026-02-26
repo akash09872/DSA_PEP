415a36dfc341761c7793a262fc1901c3dfde5284
Akash Deep <akashdeep6632@gmail.com>
2026-02-26 12:43:46 +0530

    Leet Questions

--- Practice/LeetCode/Graph/207.cpp
+++ Practice/LeetCode/Graph/207.cpp
@@ -1,35 +1,34 @@
-// https://www.geeksforgeeks.org/problems/prerequisite-tasks/1
+// https://leetcode.com/problems/course-schedule/
+
 
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    bool isPossible(int N, int P, vector<pair<int, int> >& prerequisites) {
-        // Code here
-        vector<vector<int>> adj(N);
-        vector<int> indeg(N,0);
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indeg(numCourses,0);
         for(auto i:prerequisites){
-            int v=i.first;
-            int u=i.second;
+            int v=i[1];
+            int u=i[0];
             adj[u].push_back(v);
             indeg[v]++;
         }
+        vector<int> res;
         queue<int> q;
-        for(int i=0;i<N;i++){
+        for(int i=0;i<numCourses;i++){
             if(indeg[i]==0) q.push(i);
         }
-        int count=0;
         while(!q.empty()){
-            count++;
             int k=q.front();
+            res.push_back(k);
             for(int i:adj[k]){
                 indeg[i]--;
                 if(indeg[i]==0) q.push(i);
             }
             q.pop();
         }
-        if(count!=N) return false;
+        if(res.size()!=numCourses) return false;
         return true;
-
     }
 };

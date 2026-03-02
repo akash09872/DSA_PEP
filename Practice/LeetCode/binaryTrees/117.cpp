805650c5295a9ad9b4d871549978d47c1772d25a
Akash Deep <akashdeep6632@gmail.com>
2026-03-02 22:39:06 +0530

    Practice Q

---
+++ Practice/LeetCode/binaryTrees/117.cpp
@@ -0,0 +1,42 @@
+
+// leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root)return root;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            int s=q.size();
+            for(int i=0;i<s;i++){
+                Node* t=q.front();
+                q.pop();
+                if(i==s-1) t->next=NULL;
+                else t->next=q.front();
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+            }
+        }
+        return root;
+    }
+};
